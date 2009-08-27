92dbccf4c06f2e4e1673dd980bb1667c50101a12
dasrick <dasrick@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-27 08:52:21 +0000

    - new tmp-Icon and some minor edits
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tox_googleajaxlibraries/trunk@23713 735d13b6-9817-0410-8766-e36946ffe9aa

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 
-Feel free to add some documentation or simply add a link to the online manual.
+The extension makes use of the Google AJAX Libraries API.
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tox_googleajaxlibraries"
 #
-# Auto generated 27-08-2009 09:16
+# Auto generated 27-08-2009 10:51
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'tox | Google AJAX Libraries',
-	'description' => 'This extension include libraries that are provided by Google AJAX Libraries API.',
+	'description' => 'The extension makes use of the Google AJAX Libraries API.',
 	'category' => 'misc',
 	'author' => 'Enrico Hoffmann',
 	'author_email' => 'dasrick@gmail.com',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"3fa8";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"0a28";s:14:"ext_tables.php";s:4:"6d73";s:19:"doc/wizard_form.dat";s:4:"edcb";s:20:"doc/wizard_form.html";s:4:"45de";s:43:"pi1/class.tx_toxgoogleajaxlibraries_pi1.php";s:4:"830f";s:48:"static/tox___google_ajax_libraries/constants.txt";s:4:"fb0d";s:44:"static/tox___google_ajax_libraries/setup.txt";s:4:"f4df";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"3fa8";s:10:"README.txt";s:4:"ea38";s:12:"ext_icon.gif";s:4:"6c2b";s:17:"ext_localconf.php";s:4:"0a28";s:14:"ext_tables.php";s:4:"45de";s:43:"pi1/class.tx_toxgoogleajaxlibraries_pi1.php";s:4:"ccc3";s:44:"static/tox_googleajaxlibraries/constants.txt";s:4:"1bed";s:40:"static/tox_googleajaxlibraries/setup.txt";s:4:"bfba";}',
+	'suggests' => array(
+	),
 );
 
 ?>
